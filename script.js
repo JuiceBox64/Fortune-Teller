e4fa3cc41fd401af6271259d27142d0ae4233cdd
Sean <43610852+JuiceBox64@users.noreply.github.com>
2019-03-05 09:20:23 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+$("button").click(function(){
+    var age = $(".age").val();
+    $("#age").text(2050-parseInt(age));
+    
+    var number = $(".number").val();
+    $("#number").text(number);
+    
+    var car = $(".car").val();
+    $("#car").text(car);
+    
+    var animal = $(".animal").val();
+    $("#animal").text(animal);
+    
+    var color = $(".color").val();
+    $("#color").text(color);
+    
+    var liquid = $(".liquid").val();
+    $(".liquid").text(liquid);
+    
+    var number2 = $(".number2").val();
+    $(".number2").text(number2);
+    
+    var actor = $(".actor").val();
+    $("#actor").text(actor);
+    
+    var song = $(".song").val();
+    $("#song").text(song);
+    
+    var nickname = $(".nickname").val();
+    $(".nickname").text(nickname);
+    
+    var time = $(".time").val();
+    $("#time").text(time);
+    
+});
